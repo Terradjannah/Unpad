8e9cd9af7118118f3ccdf326943b40a6f5feaedb
Terradjannah <adiajafeb@gmail.com>
2020-11-16 10:04:09 +0700

    senen 16 november 2020

---
+++ coding progdas/array/array.cpp
@@ -0,0 +1,14 @@
+#include <math.h>
+#include <conio.h>
+#include <stdio.h>
+
+main(){
+    
+    int i, nama[5] = {90, 80, 70, 60, 78};
+
+    i = nama
+    for(i=0, i<5, ++i){
+        printf();
+    }
+
+}
